056f5564d2a6a3eeb8c88b88a509edff71f89dca
Ali Mohamed Ali <3ligraphy@gmail.com>
2025-01-24 21:41:38 +0200

    fix for testfligh

---
+++ ios/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\amoha\Documents\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\amoha\Desktop\SPEAKO KUWAIT MINI PROJECTS 2025\fire prevention\fire_prevention_program\ios"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
